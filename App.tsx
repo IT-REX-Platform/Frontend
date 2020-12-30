a0778bc7ca0d435a6bc742532040935ee9582dfb
GarondEisenfaust <chr.schr@web.de>
2020-12-30 13:01:24 +0100

    Fixed it:)

--- App.tsx
+++ App.tsx
@@ -17,13 +17,19 @@ export default function App() {
 const onMouseOver = (event) => {
   const el = event.target;
   let colorhex = [
-    
+    "#7AF377",
+    "#3498DB",
+    "#F1C530",
+    "#F29C29",
     "#8E44AD",
     "#4AA086",
     "#E74C3C",
     "#65CC71",
     "#D3541B",
-    "
+    "#EB4367",
+    "#74F7D9",
+    "#DDA8FC",
+  ];
   el.style.color = colorhex[Math.floor(Math.random() * 12)];
 };
 
