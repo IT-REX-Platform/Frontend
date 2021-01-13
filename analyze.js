2701f6fbe546f1512c0e83e03e9551a8d1a0a869
nemampojma5 <marko.kovacic@ms.informatik.uni-stuttgart,de>
2021-01-13 15:46:06 +0100

    add name and url again to sonarqube params

--- analyze.js
+++ analyze.js
@@ -2,8 +2,11 @@ const scanner = require("sonarqube-scanner");
 
 scanner(
     {
+        serverUrl: "https://localhost:9001",
         // this example uses local instance of SQ
         options: {
+            "sonar.projectName": "Frontend",
+            "sonar.projectDescription": "Frontend",
             "sonar.projectVersion": "1.1.0",
             "sonar.sources": "src/",
             "sonar.tests": "__tests__/",
