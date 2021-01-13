492341eb1532ced55543c19ef216a95b0d0a601b
nemampojma5 <marko.kovacic@ms.informatik.uni-stuttgart,de>
2021-01-13 16:28:46 +0100

    add key for sonarqube

--- analyze.js
+++ analyze.js
@@ -2,11 +2,12 @@ const scanner = require("sonarqube-scanner");
 
 scanner(
     {
-        serverUrl: "http://129.69.217.173:9001/",
+        serverUrl: "",
         token: "0bd29dad78b1ed1a5436a3b3ad042df1c78d4f34",
 
         // this example uses local instance of SQ
         options: {
+            "sonar.projectKey": "Frontend",
             "sonar.projectName": "Frontend",
             "sonar.projectDescription": "Frontend",
             "sonar.projectVersion": "1.1.0",
