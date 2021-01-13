532e91d326f0e469e85a87711e271e68c99f54ca
nemampojma5 <marko.kovacic@ms.informatik.uni-stuttgart,de>
2021-01-13 17:02:07 +0100

    see what happens with less sonar params

--- analyze.js
+++ analyze.js
@@ -2,9 +2,6 @@ const scanner = require("sonarqube-scanner");
 
 scanner(
     {
-        serverUrl: "",
-        token: "0bd29dad78b1ed1a5436a3b3ad042df1c78d4f34",
-
         // this example uses local instance of SQ
         options: {
             "sonar.projectKey": "Frontend",
