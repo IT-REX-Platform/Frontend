664e257444626169713bc5894d2498c6b51c981a
dgwetzel <dani.wetzel@gmx.de>
2021-01-16 15:30:05 +0100

    update analyze.js

--- analyze.js
+++ analyze.js
@@ -8,10 +8,9 @@ scanner(
             "sonar.projectName": "Frontend",
             "sonar.projectDescription": "Frontend",
             "sonar.projectVersion": "1.1.0",
-            "sonar.sources": "src/",
-            "sonar.tests": "__tests__/",
-            "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
-            "sonar.testExecutionReportPaths": "coverage/test-reporter.xml",
+            "sonar.sources": "src",
+            "sonar.tests": "__tests__",
+            "sonar.inclusions": "App.tsx",
         },
     },
 
