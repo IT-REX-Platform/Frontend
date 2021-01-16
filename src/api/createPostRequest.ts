b9b2c6badb5b2edecd55cb9b68d70d2366beef53
dgwetzel <dani.wetzel@gmx.de>
2021-01-16 15:14:36 +0100

    codesmell

--- src/api/createPostRequest.ts
+++ src/api/createPostRequest.ts
@@ -27,6 +27,7 @@ export function createPostRequest(object: Course | Media): RequestInit {
 
 export function createRequest(): RequestInit {
     const authService = AuthenticationService.getInstance();
+    let bla = "";
     return {
         headers: {
             Authorization: authService.getToken().tokenType + " " + authService.getToken().accessToken,
