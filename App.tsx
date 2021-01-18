57eddde21840d29fd0b2edaff98d1cf083539ffc
Anna <anna.riesch@t-online.de>
2021-01-18 18:25:30 +0100

    Added Upload Video Component

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { View, Linking } from "react-native";
 import { CreateCourseComponent } from "./src/components/CreateCourseComponent";
 import { LoginComponent } from "./src/components/LoginComponent";
+import { UploadVideoComponent } from "./src/components/UploadVideoComponent";
 import TestComponent from "./src/components/TestComponent";
 
 export default function App(): JSX.Element {
@@ -16,6 +17,7 @@ export default function App(): JSX.Element {
         <View>
             <LoginComponent></LoginComponent>
             <CreateCourseComponent></CreateCourseComponent>
+            <UploadVideoComponent></UploadVideoComponent>
             <TestComponent></TestComponent>
         </View>
     );
---
+++ src/components/UploadVideoComponent.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { StyleSheet, Button, Pressable, TextInput, Text, View } from "react-native";
+
+export const UploadVideoComponent = () => {
+    const [setVideo] = useState("");
+
+    return (
+        <>
+            <View style={styles.container}>
+                <View style={styles.StyledInputContainer}>
+                    <Text>Upload Video here:</Text>
+                    <TextInput defaultValue="Document here" style={styles.StyledTextInput}></TextInput>
+                </View>
+                <Pressable style={styles.StyledButton}>
+                    <Button title="Browse Files" onPress={changeStyle}></Button>
+                </Pressable>
+            </View>
+        </>
+    );
+
+    function changeStyle() {}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+        marginBottom: 20,
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    StyledInputContainer: {
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    StyledTextInput: {
+        marginLeft: 8,
+        borderColor: "lightgray",
+        borderWidth: 2,
+    },
+    StyledButton: {
+        marginTop: 16,
+    },
+});
