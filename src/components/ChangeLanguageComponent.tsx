1cd19862dadf0d15c036b180138e1010cdf61f51
Niklas Meißner <nik.meissner@web.de>
2021-01-25 01:25:04 +0100

    ITREX-369
    preparation of language change when switch is toggled

--- src/components/ChangeLanguageComponent.tsx
+++ src/components/ChangeLanguageComponent.tsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
-import { View, Switch, StyleSheet, SafeAreaView, Text } from "react-native";
+import { Switch, StyleSheet, SafeAreaView, Text } from "react-native";
 import i18n from "../locales/index";
 
 export default function App() {
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+    var boolSwitch;
+
+    if (i18n.locale == "de" || i18n.locale == "de-DE") {
+        boolSwitch = true;
+    } else {
+        boolSwitch = false;
+    }
 
     return (
         <SafeAreaView style={styles.container}>
             <Text>{i18n.t("itrex.languageSwitch")}</Text>
             <Switch
                 trackColor={{ false: "#767577", true: "#767577" }}
-                thumbColor={isEnabled ? "#f4f3f4" : "#f4f3f4"}
+                thumbColor={boolSwitch ? "#f4f3f4" : "#f4f3f4"}
                 ios_backgroundColor="#767577"
                 onValueChange={toggleSwitch}
-                value={isEnabled}
+                value={boolSwitch}
             />
         </SafeAreaView>
     );
