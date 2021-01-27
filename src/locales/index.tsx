76eb103ae53357c1ea79e3fc139e858725199a16
Niklas Meißner <nik.meissner@web.de>
2021-01-27 13:57:17 +0100

    ITREX-369
    implemented language change via button click

--- src/locales/index.tsx
+++ src/locales/index.tsx
@@ -1,9 +1,9 @@
-import * as Localization from "expo-localization";
 import i18n from "i18n-js";
 
 import en from "./en";
 import de from "./de";
 
+// mapping of language codes to files (en/de)
 i18n.translations = {
     default: en,
     "de-DE": de,
@@ -13,7 +13,6 @@ i18n.translations = {
     en: en,
 };
 
-i18n.locale = Localization.locale;
 i18n.fallbacks = true;
 
 export default i18n;
