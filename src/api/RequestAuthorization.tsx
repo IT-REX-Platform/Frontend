958619cdb0b72d0374e79d848694c4d17abd6c4a
Pastuchov <swjatoslaw.pastuchov@gmx.de>
2021-01-30 13:20:14 +0100

    - Catch error when missing user login.

--- src/api/RequestAuthorization.tsx
+++ src/api/RequestAuthorization.tsx
@@ -1,13 +1,21 @@
 import AuthenticationService from "../services/AuthenticationService";
+import { loggerFactory } from "../../logger/LoggerConfig";
 
 export class RequestAuthorization {
+    private static loggerApi = loggerFactory.getLogger("API.RequestAuthorization");
+
     public static createAuthorizedRequest(): RequestInit {
         const authService = AuthenticationService.getInstance();
-        return {
-            headers: {
-                Authorization: authService.getToken().tokenType + " " + authService.getToken().accessToken,
-            },
-            credentials: "include",
-        };
+        try {
+            return {
+                headers: {
+                    Authorization: authService.getToken().tokenType + " " + authService.getToken().accessToken,
+                },
+                credentials: "include",
+            };
+        } catch (error) {
+            this.loggerApi.error("An error occured while getting authorizarion tokens for request.", error);
+            throw new Error("User must log in.");
+        }
     }
 }
