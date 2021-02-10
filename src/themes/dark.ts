30b01a7fe781e0ca59d4b31d19386f3bffabc599
Katja <kat.se.12@googlemail.com>
2021-02-10 10:51:05 +0100

    Added color palette for dark and ligth mode

---
+++ src/themes/dark.ts
@@ -0,0 +1,16 @@
+const dark = {
+    theme: {
+        darkBlue1: "#011B45",
+        darkBlue2: "#012B56",
+        darkBlue3: "#003B66",
+        darkBlue4: "#305880",
+        lightBlue: "#1F9BB3",
+        blueGreen: "#4FAFA7",
+        darkGreen: "#7FC29B",
+        lightGreen: "#B5EF8A",
+        grey: "#464A5B",
+        pink: "#FA1772",
+    },
+};
+
+export default dark;
---
+++ src/themes/ligth.ts
@@ -0,0 +1,11 @@
+const light = {
+    theme: {
+        orange: "#ED6A5A",
+        lightYellow: "#F4F1BB",
+        lightBlue: "#9BC1BC",
+        blueGreen: "#5CA4A9",
+        beige: "#E6EBE0",
+    },
+};
+
+export default light;
