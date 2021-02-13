9a7b8ea81d748a58fdaa69d4cfd1b14bdc6bc2d0
dgwetzel <dani.wetzel@gmx.de>
2021-02-13 19:27:54 +0100

    test AuthenticationService

--- __tests__/services/AuthenticationService.test.ts
+++ __tests__/services/AuthenticationService.test.ts
@@ -25,4 +25,24 @@ describe("test Authentication service", () => {
 
         expect(authService.getToken()).toEqual(tokenResponse);
     });
+
+    it("check if roles are present", () => {
+        global.atob = jest.fn().mockReturnValue("{name:'myCoolToken', roles: ['ITREX_LECTURER']}");
+
+        JSON.parse = jest.fn().mockImplementationOnce(() => {
+            return { name: "myCoolToken", roles: ["ITREX_LECTURER"] };
+        });
+
+        // btoa("{name:'myCoolToken', roles: ['ITREX_LECTURER']}")
+        const myToken = "hallo." + "e25hbWU6J215Q29vbFRva2VuJywgcm9sZXM6IFsnSVRSRVhfTEVDVFVSRVInXX0=" + ".hallo";
+        const tokenResponseConfig: TokenResponseConfig = {
+            accessToken: myToken,
+        };
+
+        const tokenResponse: TokenResponse = new TokenResponse(tokenResponseConfig);
+        const authService = AuthenticationService.getInstance();
+        authService.setTokenResponse(tokenResponse);
+
+        expect(authService.getRoles()).toContain("ITREX_LECTURER");
+    });
 });
