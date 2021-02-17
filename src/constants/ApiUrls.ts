17d5bb6326f0a97d8297b4e391beb42d1fe38b5f
GarondEisenfaust <chr.schr@web.de>
2021-02-17 15:00:47 +0100

    Removed extened route of mediaservice

--- src/constants/ApiUrls.ts
+++ src/constants/ApiUrls.ts
@@ -2,5 +2,5 @@ export class ApiUrls {
     // Should not be used by lecturers and students, only by admins.
     // public static URL_COURSES = "services/courseservice/api/courses";
     public static URL_COURSES = "services/courseservice/api/courses";
-    public static URL_VIDEOS_EXTENDED = "services/mediaservice/api/extended/videos";
+    public static URL_VIDEOS_EXTENDED = "services/mediaservice/api/videos";
 }
