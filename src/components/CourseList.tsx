7bb350789ca150f5d5d5ee169e1aca91332e95a7
Katja <kat.se.12@googlemail.com>
2021-02-23 18:08:43 +0100

    case distinction: Join a Course/Add a course for user role if no course exists

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, Text } from "react-native";
 import { CourseCard } from "./CourseCard";
 import { ICourse } from "../types/ICourse";
 import { MaterialIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import { NavigationRoutes } from "../constants/navigators/NavigationRoutes";
 import { dark } from "../constants/themes/dark";
+import AuthenticationService from "../services/AuthenticationService";
+import { ITREXRoles } from "../constants/ITREXRoles";
 
 interface CourseListProps {
     courses: ICourse[];
@@ -20,20 +22,44 @@ export const CourseList: React.FC<CourseListProps> = (props) => {
 function getList(courses: ICourse[]) {
     const navigation = useNavigation();
     if (courses === undefined || courses.length === 0) {
-        return (
-            <View style={styles.cardView}>
-                <Button
-                    color={dark.theme.blueGreen}
-                    title="Create a new Course!"
-                    onPress={() => navigation.navigate(NavigationRoutes.ROUTE_CREATE_COURSE)}></Button>
-            </View>
-        );
+        return <> {getUserPossibility()}</>;
     } else if (courses.length > 0) {
         return (
             <View style={styles.cardView}>
                 {courses.map((course) => {
                     return <CourseCard course={course} />;
                 })}
+            </View>
+        );
+    }
+}
+
+function getUserPossibility() {
+    const navigation = useNavigation();
+
+    if (
+        AuthenticationService.getInstance().getRoles().includes(ITREXRoles.ROLE_LECTURER) ||
+        AuthenticationService.getInstance().getRoles().includes(ITREXRoles.ROLE_ADMIN)
+    ) {
+        console.log("ICH BIN EIN LECTURER");
+        return (
+            <View style={styles.cardView}>
+                <Button
+                    color={dark.theme.blueGreen}
+                    title="Create a new Course!"
+                    onPress={() => navigation.navigate(NavigationRoutes.ROUTE_CREATE_COURSE)}
+                />
+            </View>
+        );
+    } else if (AuthenticationService.getInstance().getRoles().includes(ITREXRoles.ROLE_STUDENT)) {
+        console.log("ICH BIN EIN STUDENT");
+        return (
+            <View style={styles.cardView}>
+                <Button
+                    color={dark.theme.blueGreen}
+                    title="JOIN A COUSE!"
+                    onPress={() => navigation.navigate(NavigationRoutes.ROUTE_CREATE_COURSE)}
+                />
             </View>
         );
     }
--- src/components/screens/ScreenHomeStudent.tsx
+++ src/components/screens/ScreenHomeStudent.tsx
@@ -1,28 +1,136 @@
-import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { CourseList } from "../CourseList";
+import { courseList } from "../../constants/fixtures/courseList.fixture";
 import i18n from "../../locales";
+import Select from "react-select";
+import { ICourse } from "../../types/ICourse";
 import { Header } from "../../constants/navigators/Header";
 import { LocalizationContext } from "../Context";
 
 export const ScreenHomeStudent: React.FC = () => {
     React.useContext(LocalizationContext);
+    const navigation = useNavigation();
+
+    const [filteredCourses, setFilteredCourses] = useState<ICourse[]>([]);
+
+    const [selectedPublishStateFilter, setPublishStateFilter] = useState<string | undefined>("PUBLISHED");
+    const [selectedActiveState, setSelectedActiveState] = useState<string | undefined>("ACTIVE");
+
+    useEffect(() => {
+        if (selectedPublishStateFilter === undefined) {
+            return;
+        }
+        if (selectedActiveState === undefined) {
+            return;
+        }
+        const courseList: ICourse[] = fetchCourses(selectedPublishStateFilter, selectedActiveState);
+        setFilteredCourses(courseList);
+    }, [selectedPublishStateFilter, selectedActiveState]);
+
+    function filter() {
+        if (filteredCourses === undefined) {
+            return;
+        } else if (filteredCourses.length > 0) {
+            return (
+                <View style={{ flexDirection: "row", zIndex: 1 }}>
+                    <View style={{ width: 300, zIndex: 3 }}>
+                        <Text style={{ color: "white" }}>Filter for published/unpublished courses</Text>
+                        <Select
+                            options={publishStateFilterOptions}
+                            defaultValue={publishStateFilterOptions[0]}
+                            onChange={(option) => {
+                                setPublishStateFilter(option?.value);
+                            }}
+                        />
+                    </View>
+                    <View style={{ width: 300, zIndex: 3 }}>
+                        <Text style={{ color: "white" }}>Filter for active/inactive courses</Text>
+                        <Select
+                            options={activeStateFilterOptions}
+                            defaultValue={activeStateFilterOptions[0]}
+                            onChange={(option) => {
+                                setSelectedActiveState(option?.value);
+                            }}
+                        />
+                    </View>
+                </View>
+            );
+        }
+    }
 
     return (
         <View style={styles.container}>
+            <Header title={i18n.t("itrex.home")} />
             <ImageBackground source={require("../../constants/images/Background2.png")} style={styles.image}>
-                <Header title={i18n.t("itrex.home")} />
-                <View style={styles.textContainer}>
-                    <Image
-                        source={require("../../constants/images/ITRex-Logo-ob_750x750.png")}
-                        style={[styles.icon]}></Image>
-                    <Text style={styles.textSytle}>{i18n.t("itrex.welcome")} Student</Text>
-                    <Text style={styles.textSytle}>{i18n.t("itrex.starter")}</Text>
-                </View>
+                <Text style={{ color: "white" }}>{i18n.t("itrex.homeStudentText")}</Text>
+                {filter()}
+                <CourseList courses={filteredCourses} />
             </ImageBackground>
         </View>
     );
 };
+
+const publishStateFilterOptions = [
+    { value: "PUBLISHED", label: "Published" },
+    { value: "UNPUBLISHED", label: "Unpublished" },
+    { value: "ALL", label: "All" },
+];
+
+const activeStateFilterOptions = [
+    { value: "ACTIVE", label: "Active" },
+    { value: "INACTIVE", label: "Inactive" },
+    { value: "ALL", label: "All" },
+];
+
+//TODO: Replace with real endpoint-logic and export to other file
+function fetchCourses(publishState: string, setSelectedActiveState: string): ICourse[] {
+    // initial : all relevant courses
+    let filteredCourses: ICourse[] = courseList;
+    if (publishState !== "ALL") {
+        filteredCourses = getMatchingPublishStateCourses(publishState, filteredCourses);
+    }
+    if (setSelectedActiveState !== "ALL") {
+        filteredCourses = getMatchingActivityStateCourses(setSelectedActiveState, filteredCourses);
+    }
+    return filteredCourses;
+}
+
+//TODO: Replace with real endpoint-logic
+function getMatchingPublishStateCourses(publishState: string, inputCourseList: ICourse[]): ICourse[] {
+    const filteredCourses: ICourse[] = [];
+
+    for (const course of inputCourseList) {
+        if (course.publishState === publishState) {
+            filteredCourses.push(course);
+        }
+    }
+    return filteredCourses;
+}
+
+//TODO: Replace with real endpoint-logic and export to other file
+// eslint-disable-next-line complexity
+function getMatchingActivityStateCourses(activeState: string, inputCourseList: ICourse[]): ICourse[] {
+    const activeCourses: ICourse[] = [];
+    const inactiveCourses: ICourse[] = [];
+
+    // Curr date
+    const offset = new Date();
+    // Offset = Curr date + 8 weeks
+    offset.setDate(offset.getDate() + 8 * 7);
+
+    for (const course of inputCourseList) {
+        if (course.endDate && course.endDate <= offset) {
+            activeCourses.push(course);
+        } else {
+            inactiveCourses.push(course);
+        }
+    }
+
+    return activeState === "ACTIVE" ? activeCourses : inactiveCourses;
+}
 
 const styles = StyleSheet.create({
     textContainer: {
