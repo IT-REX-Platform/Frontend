b18163308f4f10b95e7223fb9cb158bd47c03faa
GraMMa <marcel@gramma.name>
2021-03-08 12:40:36 +0100

    Join Component refreshes token + userinfo on focus

--- src/components/JoinCourseComponent.tsx
+++ src/components/JoinCourseComponent.tsx
@@ -5,7 +5,7 @@ import { ICourse } from "../types/ICourse";
 import i18n from "../locales";
 import { RequestFactory } from "../api/requests/RequestFactory";
 import { EndpointsCourse } from "../api/endpoints/EndpointsCourse";
-import { RouteProp, useNavigation } from "@react-navigation/native";
+import { RouteProp, useIsFocused, useNavigation } from "@react-navigation/native";
 import { Header } from "../constants/navigators/Header";
 import { LocalizationContext } from "./Context";
 import { NavigationRoutes, RootDrawerParamList } from "../constants/navigators/NavigationRoutes";
@@ -32,6 +32,17 @@ export const JoinCourseComponent: React.FC = () => {
     useEffect(() => {
         AuthenticationService.getInstance().getUserInfo(setUserInfo);
     }, []);
+
+    const isFocused = useIsFocused();
+    useEffect(() => {
+        if (isFocused) {
+            AuthenticationService.getInstance()
+                .refreshToken()
+                .then(() => {
+                    AuthenticationService.getInstance().getUserInfo(setUserInfo);
+                });
+        }
+    }, [isFocused]);
 
     return (
         <ImageBackground source={require("../constants/images/Background2.png")} style={styles.imageContainer}>
@@ -83,9 +94,13 @@ export const JoinCourseComponent: React.FC = () => {
         const request: RequestInit = RequestFactory.createPostRequestWithoutBody();
         endpointsCourse.joinCourse(request, courseId);
 
-        navigation.navigate(NavigationRoutes.ROUTE_COURSE_DETAILS, {
-            courseId: courseId,
-        });
+        AuthenticationService.getInstance()
+            .refreshToken()
+            .then(() => {
+                navigation.navigate(NavigationRoutes.ROUTE_COURSE_DETAILS, {
+                    courseId: courseId,
+                });
+            });
     }
 };
 
