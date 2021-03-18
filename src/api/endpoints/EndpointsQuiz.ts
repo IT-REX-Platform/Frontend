6ea082f2d205d459256b0781ec82f6938048ae42
Pastuchov <swjatoslaw.pastuchov@gmx.de>
2021-03-18 17:52:38 +0100

    - Added quiz endpoints docs.

--- src/api/endpoints/EndpointsQuiz.ts
+++ src/api/endpoints/EndpointsQuiz.ts
@@ -21,12 +21,29 @@ export class EndpointsQuiz implements IEndpointsQuiz {
         this.responseParser = new ResponseParser();
     }
 
+    /**
+     * Get one or more quizzes.
+     *
+     * @param getRequest GET request.
+     * @param successMsg A success message.
+     * @param errorMsg An error message.
+     * @returns
+     */
     getAllQuizzes(getRequest: RequestInit, successMsg?: string, errorMsg?: string): Promise<IQuiz[]> {
         this.loggerApi.trace("Sending GET request to URL: " + this.url);
         const response: Promise<Response> = sendRequest(this.url, getRequest);
         return this.responseParser.parseQuizzes(response, successMsg, errorMsg);
     }
 
+    /**
+     * Get all quizzes of a course.
+     *
+     * @param getRequest GET request.
+     * @param courseId ID of course to which the quiz belongs.
+     * @param successMsg A success message.
+     * @param errorMsg An error message.
+     * @returns
+     */
     getCourseQuizzes(
         getRequest: RequestInit,
         courseId: string,
@@ -40,6 +57,15 @@ export class EndpointsQuiz implements IEndpointsQuiz {
         return this.responseParser.parseQuizzes(response, successMsg, errorMsg);
     }
 
+    /**
+     * Get one quiz.
+     *
+     * @param getRequest GET request.
+     * @param quizId ID of a quiz.
+     * @param successMsg A success message.
+     * @param errorMsg An error message.
+     * @returns
+     */
     getQuiz(
         getRequest: RequestInit,
         quizId: string,
@@ -53,20 +79,47 @@ export class EndpointsQuiz implements IEndpointsQuiz {
         return this.responseParser.parseQuiz(response, successMsg, errorMsg);
     }
 
+    /**
+     * Create a new quiz.
+     *
+     * @param postRequest POST request with quiz JSON body.
+     * @param successMsg A success message.
+     * @param errorMsg An error message.
+     * @returns
+     */
     createQuiz(postRequest: RequestInit, successMsg?: string, errorMsg?: string): Promise<IQuiz | undefined> {
         //TODO: ADD  QUIZ TO CONTENT TO THE CHAPTER;
+        // TODO: eh?
 
         this.loggerApi.trace("Sending POST request to URL: " + this.url);
         const response: Promise<Response> = sendRequest(this.url, postRequest);
         return this.responseParser.parseQuiz(response, successMsg, errorMsg);
     }
 
+    /**
+     * Update a quiz.
+     *
+     * @param putRequest PUT request with quiz JSON body
+     * @param successMsg A success message.
+     * @param errorMsg An error message.
+     * @returns
+     */
     updateQuiz(putRequest: RequestInit, successMsg?: string, errorMsg?: string): Promise<IQuiz | undefined> {
         this.loggerApi.trace("Sending PUT request to URL: " + this.url);
         const response: Promise<Response> = sendRequest(this.url, putRequest);
         return this.responseParser.parseQuiz(response, successMsg, errorMsg);
     }
 
+    /**
+     * Delete a quiz.
+     *
+     * @param deleteRequest DELETE request.
+     * @param quizId ID of a quiz.
+     * @param withQuestions Boolean determines whether the quiz is deleted with all its questions or not.
+     * @param successMsg A success message.
+     * @param errorMsg An error message.
+     * @returns
+     */
     deleteQuiz(
         deleteRequest: RequestInit,
         quizId: string,
