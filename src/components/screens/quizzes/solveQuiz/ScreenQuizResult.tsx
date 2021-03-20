0cca06aec9c3d73a2b4ba120162bac1d03a1e363
Marko <markokovacic@hotmail.de>
2021-03-20 15:17:24 +0100

    Evaluate number of correct questions

--- src/components/screens/quizzes/solveQuiz/ScreenQuizResult.tsx
+++ src/components/screens/quizzes/solveQuiz/ScreenQuizResult.tsx
@@ -5,6 +5,7 @@ import { TextButton } from "../../../uiElements/TextButton";
 import { useNavigation } from "@react-navigation/core";
 import { RouteProp, useRoute } from "@react-navigation/native";
 import { CourseStackParamList } from "../../../../constants/navigators/NavigationRoutes";
+import { quizList } from "../../../../constants/fixtures/quizzes.fixture";
 
 type ScreenQuizResultProps = RouteProp<CourseStackParamList, "QUIZ_RESULT">;
 
@@ -12,12 +13,16 @@ export const ScreenQuizResult: React.FC = () => {
     const route = useRoute<ScreenQuizResultProps>();
     const quiz: IQuiz = route.params.quiz;
 
+    console.log(quiz);
+
     const navigation = useNavigation();
     return (
         <>
             <Text>Quiz Solution Screen</Text>
             <Text>{quiz.name}</Text>
-            <Text>You solved X out of {quiz.questions.length} questions</Text>
+            <Text>
+                You solved {correctlySolved()} out of {quiz.questions.length} questions
+            </Text>
 
             <TextButton
                 title={"Return to chapter"}
@@ -25,4 +30,18 @@ export const ScreenQuizResult: React.FC = () => {
                 onPress={() => navigation.navigate("INFO", { screen: "OVERVIEW" })}></TextButton>
         </>
     );
+
+    function correctlySolved(): number {
+        let amountCorrectlySolved = 0;
+
+        quiz.questions.map((question) => {
+            if (JSON.stringify(question.solution) === JSON.stringify(question.userInput)) {
+                amountCorrectlySolved += 1;
+            } else {
+                console.log("wrong", question);
+            }
+        });
+
+        return amountCorrectlySolved;
+    }
 };
